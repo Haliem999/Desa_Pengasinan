72164adaecf448ae40d02192fb4f58c67087967c
Haliem999 <144505834+Haliem999@users.noreply.github.com>
2023-10-28 19:45:37 -0700

    Update Banner.vue

--- src/components/Banner.vue
+++ src/components/Banner.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="green-600 h-[300px] flex">
+  <div class="light sea green-600 h-[300px] flex">
     <img
       src="@/assets/logo-bogor.png"
       class="w-45 h-50 opacity-60 m-20"
